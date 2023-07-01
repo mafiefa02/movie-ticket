fac5aa359616d50025554a239f9557ed74b43687
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-07-02 02:03:30 +0800

    Initialize react-query SSR method

---
+++ src/lib/hydrate.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import { Hydrate as Hidrasi, HydrateProps } from "@tanstack/react-query";
+
+export function Hydrate({ children, ...props }: HydrateProps) {
+    return <Hidrasi {...props}>{children}</Hidrasi>
+}
