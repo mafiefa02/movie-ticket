90849a1df1ac6f80db9cbd0eabf0b548b6fd3f00
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-07-02 14:30:13 +0800

    add api route to search movie by title

---
+++ src/app/api/movies/get-movies-by-title/route.ts
@@ -0,0 +1,21 @@
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const title = headers().get("title")?.toString();
+  if (!title)
+    return NextResponse.json({ error: "No title provided" }, { status: 400 });
+
+  const titleURL = title.replace(" ", "%20").replace("&", "%26");
+  const movieBackdrop = await fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${titleURL}`,
+    {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+      },
+    }
+  ).then((res) => res.json());
+
+  return NextResponse.json(movieBackdrop.results[0].backdrop_path);
+}
