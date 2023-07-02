1a1c3ec17cd9df8fe6bdc3a1a930d16fab78fc54
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-07-02 15:26:10 +0800

    hopefully fixed

--- src/app/(sections)/hero.tsx
+++ src/app/(sections)/hero.tsx
@@ -54,14 +54,18 @@ export default function Hero({ movie, tmdbMovieAPI }: HeroProps) {
     if (isLoading) return <p>Loading...</p>
     if (isError) return <p>{error.message}</p>
 
-    const tmdbMovie = data.results[0]
+    const tmdbMovie = data.results.slice(0, 1)
 
     return (
         <Container className="h-[70vh]">
             <div className="absolute left-0 top-0 w-full h-screen -z-50">
                 <div className="absolute left-0 top-0 w-full h-screen bg-gradient-to-t from-background to-transparent" />
                 <div className="absolute left-0 top-0 w-full h-screen bg-gradient-to-tr from-background to-transparent" />
-                <Image src={`https://image.tmdb.org/t/p/original/${tmdbMovie.backdrop_path}`} alt={movie.title} fill style={{ objectFit: "cover", zIndex: -40 }} placeholder="blur" blurDataURL={"process.env.BLUR_DATA_URL"} />
+                {
+                    tmdbMovie.map((tmdbMovie) => {
+                        return <Image key={movie.title} src={`https://image.tmdb.org/t/p/original/${tmdbMovie.backdrop_path}`} alt={movie.title} fill style={{ objectFit: "cover", zIndex: -40 }} placeholder="blur" blurDataURL={"process.env.BLUR_DATA_URL"} />
+                    })
+                }
             </div>
             <div className="relative max-w-sm md:max-w-xl flex flex-col items-start justify-end h-full">
                 <H1 className="text-primary">{movie.title}</H1>
