d9f62d99e46740e87b05fd459e9debd3545681ba
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-07-02 18:49:25 +0800

    letts go

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,57 @@
+import { useState } from "react";
+
 import Navbar from "@/components/layout/navbar";
 import getQueryClient from "@/lib/get-query-client";
 import { Hydrate } from "@/lib/hydrate";
+import { tmdbMovie, tmdbResponse } from "@/types/tmdb";
 import { Movie } from "@prisma/client";
 import { dehydrate } from "@tanstack/react-query";
 
 import Hero from "./(sections)/hero";
 import NowPlaying from "./(sections)/now-playing";
 
-interface tmdbResponse {
-  page: number,
-  results: tmdbMovieType[],
-  total_pages: number,
-  total_results: number
-}
-interface tmdbMovieType {
-  "adult": boolean,
-  "backdrop_path": string,
-  "genre_ids": number[],
-  "id": number,
-  "original_language": string,
-  "original_title": string,
-  "overview": string,
-  "popularity": number,
-  "poster_path": string,
-  "release_date": string,
-  "title": string,
-  "video": boolean,
-  "vote_average": number,
-  "vote_count": number
-}
-
 async function getMovies() {
-  const movies = await fetch(
+  const movies: Movie[] = await fetch(
     "https://seleksi-sea-2023.vercel.app/api/movies"
   ).then((res) => res.json());
   return movies
 }
 
 async function getMovieByTitle(title: string) {
-  const titleURL = title.replace(" ", "%20").replace("&", "%26");
-  const movieBackdrop = await fetch(
-    `https://api.themoviedb.org/3/search/movie?query=${titleURL}`, {
+  const options = {
+    method: "GET",
     headers: {
       accept: "application/json",
-      Authorization: `Bearer ${process.env.TMDB_API_KEY}`
-    }
-  }
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+  };
+  const res: tmdbResponse = await fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${title}`,
+    options
   ).then((res) => res.json());
 
-  const returnValue = movieBackdrop.results[0]
+  if (res.results.length === 0) {
+    return null
+  }
 
-  return returnValue
+  return res.results[0] as tmdbMovie
 }
 
 
 export default async function Home() {
-  const movies: Movie[] = await getMovies()
-  const random = Math.floor((Math.random() * (movies.length - 1)))
-  const movie = movies[random]
-  const tmdbMovie = await getMovieByTitle(movie.title)
+  const nowPlaying = await getMovies()
+  const random = Math.floor(Math.random() * nowPlaying.length)
+  const featured = nowPlaying[random]
+  const featuredBackdrop = await getMovieByTitle(featured.title).then((res) => {
+    if (res === null) return featured.poster_url
+    return res.backdrop_path
+  })
 
   return (
     <>
       <Navbar />
-      <Hero movie={movie} tmdbMovie={tmdbMovie} />
-      <NowPlaying movies={movies} />
+      <Hero movie={featured} backdrop={featuredBackdrop} />
+      <NowPlaying movies={nowPlaying} />
     </>
   )
 }
