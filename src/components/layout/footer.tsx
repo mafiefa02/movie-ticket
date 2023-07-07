06d11db643c978ef1c2fca51aa68507304be17d1
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-07-07 11:27:52 +0800

    change footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,15 +1,31 @@
+import { Github, Instagram, LinkedinIcon, MailIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
-    <div className="flex items-center justify-center bg-primary p-3">
-      <Link href="https://afiefabdurrahman.vercel.app">
-        <p className="font-bold text-primary-foreground">
-          © {year} Afief Abdurrahman
-        </p>
-      </Link>
-    </div>
+    <footer className="relative mt-16 flex w-full flex-col items-center gap-3 bg-black px-12 py-6 md:flex-row md:justify-between">
+      <div className="flex flex-row gap-6 md:gap-4">
+        <Link href={"https://github.com/mafiefa02"}>
+          <Github color="#FFF" />
+        </Link>
+        <Link href={"https://linkedin.com/in/mafiefa"}>
+          <LinkedinIcon color="#FFF" />
+        </Link>
+        <Link href={"mailto:mafiefa.business@gmail.com"}>
+          <MailIcon color="#FFF" />
+        </Link>
+        <Link href={"https://github.com/mafiefa02"}>
+          <Github color="#FFF" />
+        </Link>
+        <Link href={"https://instagram.com/mafiefa"}>
+          <Instagram color="#FFF" />
+        </Link>
+      </div>
+      <p className="text-xs text-white md:text-base">
+        &#169; {year} Afief Abdurrahman
+      </p>
+    </footer>
   );
 }
